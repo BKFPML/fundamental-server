8da0b493d2ec32993290cff0db51a8c8751cd8a3
Kentin Paille <nathanpaillet@gmail.com>
2024-12-03 18:33:03 +0900

    feat: implement getTokenMetadata method and update getTokenBalances to include token metadata handling

--- src/alchemy/alchemy.service.ts
+++ src/alchemy/alchemy.service.ts
@@ -2,7 +2,6 @@ import { Injectable, Logger } from '@nestjs/common';
 import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
 import { log } from 'console';
-
 @Injectable()
 export class AlchemyService {
     private readonly apiKey: string;
@@ -13,8 +12,38 @@ export class AlchemyService {
         this.apiKey = this.configService.get<string>('ALCHEMY_API_KEY');
       }
     
+    public async getTokenMetadata(contractAddress: string): Promise<any> {
 
-    public async getTokenBalances(address: string): Promise<string> {
+        const network = 'base';
+        const Url = `https://${network}-mainnet.g.alchemy.com/v2/${this.apiKey}`;
+
+        const data = {
+        jsonrpc: '2.0',
+        method: 'alchemy_getTokenMetadata',
+        params: [contractAddress],
+        id: 1,
+        };
+
+        try {
+        const response = await axios.post(Url, data);
+        const metadata = response.data.result;
+
+        if (metadata) {
+            return {
+            name: metadata.name || 'Nom indisponible',
+            symbol: metadata.symbol || 'Symbole indisponible',
+            decimals: metadata.decimals || 'Decimales indisponibles',
+            };
+        } else {
+            throw new Error('Token metadata not found');
+        }
+        } catch (error) {
+        Logger.error('Error fetching token metadata:', error);
+        throw error;
+        }
+    }
+
+    public async getTokenBalances(address: string): Promise<any[]> {
         
         const network = "base"
         const Url = `https://${network}-mainnet.g.alchemy.com/v2/${this.apiKey}`;
@@ -27,10 +56,25 @@ export class AlchemyService {
         };
     
         try {
-            const response = await axios.post(Url, data);
-            const balances = response.data.result;
-        
-            return balances;
+            let response = await axios.post(Url, data);
+            let balances = response.data.result.tokenBalances;
+            
+            let answer = [];
+            
+            for (const balance of balances) {
+                // let metadata = await this.getTokenMetadata(balance.contractAddress); 
+                answer.push({
+                    asset: balance.contractAddress,
+                    // name: metadata.name,
+                    // symbol: metadata.symbol,
+                    value: balance.tokenBalance,
+                    // decimal: metadata.decimals
+                });
+                
+            }
+            
+
+            return answer;
         } catch (error) {
             console.error('Error fetching token balances:', error);
             throw error;
